8ad821e416a47523d1f2dc2becc053f35f139bc4
Nara Lee <daisyleenr@gmail.com>
2021-08-31 17:39:29 +0900

    add model, schemas and api

--- sql_app/database.py
+++ sql_app/database.py
@@ -2,4 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = ""
+SQLALCHEMY_DATABASE_URL = "mysql://admin:admin@localhost:3306/fastapi_tutorial"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
